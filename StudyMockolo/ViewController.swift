0185da559fa3e15dac53188ed3aa09229d1db8b7
daybysay <yukita.sekai+github@gmail.com>
2020-04-20 10:45:04 +0900

    initial commit

---
+++ StudyMockolo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyMockolo
+//
+//  Created by Takayuki Sei on 2020/04/20.
+//  Copyright © 2020 takayuki sei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
