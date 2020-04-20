138bea5f6ae80f91d81b39b93c1d4a463f4d72fe
daybysay <yukita.sekai+github@gmail.com>
2020-04-20 10:52:50 +0900

    add test target

---
+++ StudyMockoloTests/StudyMockoloTests.swift
@@ -0,0 +1,33 @@
+//
+//  StudyMockoloTests.swift
+//  StudyMockoloTests
+//
+//  Created by Takayuki Sei on 2020/04/20.
+//  Copyright © 2020 takayuki sei. All rights reserved.
+//
+
+import XCTest
+
+class StudyMockoloTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
